046d8ae362dd787c2766769ad6bd0597e1f6c554
Joel Christner <joel.christner@gmail.com>
2019-06-22 18:30:57 -0700

    Initial commit

---
+++ ChunkDecoder/ChunkDecoder/Decoder.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+
+namespace ChunkDecoder
+{
+    /// <summary>
+    /// Decode chunk-transfer-encoded streams or byte arrays, particularly from HTTP data.
+    /// </summary>
+    public class Decoder
+    {
+        #region Public-Members
+
+        #endregion
+
+        #region Private-Members
+
+        #endregion
+
+        #region Constructors-and-Factories
+
+        /// <summary>
+        /// Instantiate the object.
+        /// </summary>
+        public Decoder()
+        {
+
+        }
+
+        #endregion
+
+        #region Public-Methods
+
+        /// <summary>
+        /// Decode a chunk-transfer-encoded byte array to a non-chunked byte array.
+        /// </summary>
+        /// <param name="data">Original byte array with chunk-transfer-encoding.</param>
+        /// <param name="outData">Data without encoding.</param>
+        /// <returns>True if successful.</returns>
+        public bool Decode(byte[] data, out byte[] outData)
+        {
+            outData = null;
+            if (data == null || data.Length < 1) throw new ArgumentNullException(nameof(data));
+
+            MemoryStream ms = new MemoryStream();
+            ms.Write(data, 0, data.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            long contentLength = 0;
+            MemoryStream outStream = null;
+
+            if (Decode(ms, out contentLength, out outStream))
+            {
+                if (contentLength > 0)
+                {
+                    outData = new byte[contentLength];
+                    outData = outStream.ToArray();
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Decode a chunk-transfer-encoded stream to a non-chunked stream.
+        /// </summary>
+        /// <param name="stream">Original stream with chunk-transfer-encoding.</param>
+        /// <param name="contentLength">Content length of the data in the output stream.</param>
+        /// <param name="outStream">Output stream containing data without encoding.</param>
+        /// <returns>True if successful.</returns>
+        public bool Decode(Stream stream, out long contentLength, out MemoryStream outStream)
+        {
+            contentLength = 0;
+            outStream = new MemoryStream();
+
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (!stream.CanRead) throw new ArgumentException("Cannot read from supplied stream.");
+            if (stream.CanSeek) stream.Seek(0, SeekOrigin.Begin);
+
+            int bytesRead = 0;
+            long segmentLength = 0;
+            byte[] headerBuffer = new byte[1];
+            string header = "";
+            byte[] dataBuffer = null;
+            long bytesRemaining = 0;
+
+            while (true)
+            {
+                #region Read-Chunk-Length
+
+                headerBuffer = new byte[1];
+                header = "";
+
+                while (true)
+                {
+                    bytesRead = stream.Read(headerBuffer, 0, headerBuffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        header += Convert.ToChar(headerBuffer[0]);
+                        if ((int)headerBuffer[0] == 10)
+                        {
+                            // end of header
+                            break;
+                        }
+                    }
+                }
+
+                #endregion
+
+                #region Check-for-End
+
+                header = header.Trim();
+                if (!String.IsNullOrEmpty(header)) segmentLength = Convert.ToInt64(header, 16);
+                if (segmentLength < 1)  // Segment length of 0 indicates end of message
+                {
+                    // Read out the final \r\n
+                    headerBuffer = new byte[2];
+                    bytesRead = stream.Read(headerBuffer, 0, headerBuffer.Length);
+                    break;  // end of stream
+                }
+
+                #endregion
+
+                #region Read-Data
+
+                dataBuffer = new byte[segmentLength];
+                bytesRemaining = segmentLength;
+
+                while (bytesRemaining > 0)
+                {
+                    bytesRead = stream.Read(dataBuffer, 0, dataBuffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        outStream.Write(dataBuffer, 0, bytesRead);
+                        bytesRemaining -= bytesRead;
+                        contentLength += bytesRead;
+                    }
+                }
+
+                #endregion
+
+                #region Read-CRLF-After-Data
+
+                // Read out the final \r\n
+                byte[] newlineBuffer = new byte[1];
+                while (true)
+                {
+                    bytesRead = stream.Read(newlineBuffer, 0, newlineBuffer.Length);
+                    if (bytesRead > 0)
+                    {
+                        if ((int)newlineBuffer[0] == 10) break;
+                    }
+                }
+
+                #endregion
+            }
+
+            outStream.Seek(0, SeekOrigin.Begin);
+            return true;
+        }
+
+        #endregion
+
+        #region Private-Methods
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,69 @@
+# ChunkDecoder 
+
+ChunkDecoder is useful to help decode chunk-transfer-encoded data, particularly from HTTP.
+
+## New in v1.0.x
+
+- Initial release
+- Does not support signature verification of chunk data
+
+## Usage
+```
+using ChunkDecoder;
+
+public Decoder decoder = new Decoder();
+
+// process a byte array
+if (decoder.Decode(data, out outData))
+{
+  // success
+}
+
+// process a stream
+long outLength = 0;
+MemoryStream outStream = null;
+if (decoder.Decode(stream, out outLength, out outStream))
+{
+  // success
+}
+```
+
+## Functionality
+
+Chunk-transfer-encoded data is sent in segments, where each segment is comprised of a line containing the length (in hexadecimal) and the subsequent line containing the payload.  Each line ends with a carriage return and line feed <crlf>.
+
+The final line ends with ```0<crlf><crlf>``` indicating the end of the stream.
+
+For instance:
+```
+6<crlf>        // 6 bytes of data will follow
+Hello_<crlf>   // data.  The value is 'Hello_' where _ is actually a space
+5<crlf>        // 5 bytes of data will follow
+World<crlf>    // data.  The value is 'World'
+0<crlf><crlf>  // end of stream
+```
+Results in:
+```
+Hello world
+```
+
+For more information, please see: 
+
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Transfer-Encoding
+- https://en.wikipedia.org/wiki/Chunked_transfer_encoding
+
+## Limitations
+
+Chunk transfer encoding commonly includes key-value pairs in the length line, where the length and the key-value pairs are all separated by semicolons.  These key-value pairs can be used for additional processing including validation of the included segment of data.
+
+For instance:
+```
+6;chunk-signature=A917FE91...;anotherkey=anothervalue<crlf>
+```
+
+The current release of ChunkDecoder does NOT support signature validation or processing of additional key-value pairs in the length line.
+
+## Version History
+
+v1.0.x
+- Initial release
